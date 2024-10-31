3c0fc266eae37f56230b33b903a38004c70a3c02
Loïc Donot <loic.donot@haute-garonne.gouv.fr>
2024-10-31 17:27:30 +0100

    Améliorations

--- js/script.js
+++ js/script.js
@@ -34,7 +34,6 @@ $(function(){
             serieprod_conso.push([from, to, weight, color]);
         }
     }
-    console.log(serieprod_conso);
 
     Highcharts.chart('graph_prod_conso', {
 
@@ -49,33 +48,53 @@ $(function(){
             text: undefined
         },
 
-        xAxis: {
-            plotBands: [{
-                from: 0,
-                to: 200,
-                color: 'rgba(0,0,0,0.5)'
-            }],
-        },
-
         tooltip:{
+            useHTML: true,
             formatter: function() {
-                if ( this.point.isNode === true ) {
+                let tmpl = '';
+                if ( this.point.isNode === true ) { // Noeuds
+                    if (this.point.level < 2 ) { // Noeuds prod
+                        tmpl = `<strong>${this.point.name}:</strong><br><br>
+                        Production EnR: <strong style="font-size:1.8rem;">${Highcharts.numberFormat(this.point.sum)}</strong> GWh`;
+                        return tmpl;                    
+                    } else if( this.point.level == 2 ) {  // noeuds prod / conso
+                        const prod_enr = getsumweight(this.point.linksTo);
+                        const conso_enr = getsumweight(this.point.linksFrom);
+                        tmpl = `<strong>${this.point.name}:</strong>
+                        <table>
+                        <tr><td>Production EnR:</td><td style="text-align:right"><strong style="font-size:1.8rem;">${Highcharts.numberFormat(prod_enr)}</strong> GWh</td></tr>
+                        <tr><td>Consommation EnR: </td><td style="text-align:right"><strong style="font-size:1.8rem;">${Highcharts.numberFormat(conso_enr)}</strong> GWh</td></tr>
+                        </table>
+                        `;
+                        return tmpl; 
+                    } else { // Noeuds conso
+                        tmpl = `<strong>${this.point.name}:</strong><br><br>
+                        Consommation EnR: <strong style="font-size:1.8rem;">${Highcharts.numberFormat(this.point.sum)}</strong> GWh`;
+                        return tmpl;
+                    }
+                    
+                } else { // Liens entre noeuds
                     console.log(this);
-                    return Highcharts.numberFormat(this.point.sum) + ' GWh';                    
-                } else {
-                    console.log(this);
-                    return Highcharts.numberFormat(this.point.weight) + ' GWh'; 
+                    let title = 'Production EnR';
+                    if ( this.point.toNode.level == 3 ) {
+                        title = 'Consommation EnR';
+                    }
+                    tmpl = `${title}<br><strong>${this.point.from} <span style="font-size:2rem; position:relative; top:5px;">⇝</span>
+                    ${this.point.to}:</strong><br>
+                    <div style="text-align:right">
+                        <strong style="font-size:1.8rem;">${Highcharts.numberFormat(this.point.weight)}</strong>GWh
+                    </div>`;
+                    return tmpl;
                 }
             },
-            /*pointFormat: '{point.name}: {point.sum:.2f} --',*/
         },
     
         series: [{
             keys: ['from', 'to', 'weight', 'color'],
             nodes: [
-                {id:'Rural autonome', color:'#4daf4a', width: 40},
-                {id:'Rural périurbain', color:'#984ea3', width: 40},
-                {id:'Urbain', color:'#e41a1c', width: 40},
+                {id:'Rural autonome', color:'#4daf4a', width: 40, borderWidth: 2, borderColor: 'black'},
+                {id:'Rural périurbain', color:'#984ea3', width: 40, borderWidth: 2, borderColor: 'black'},
+                {id:'Urbain', color:'#e41a1c', width: 40, borderWidth: 2, borderColor: 'black'},
                 {id:'Production d\'agrocarburants', color:'#006065'},
                 {id:'Filière bois-énergie', color:'#ba7b3b'},
                 {id:'Solaire photovoltaïque', color:'#f7c631'},
@@ -99,3 +118,7 @@ $(function(){
     
 
 });
+
+function getsumweight(arr_weight){
+    return arr_weight.reduce((accumulator, current) => accumulator + current.weight, 0);
+}
