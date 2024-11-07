fe0b55c6f6b0884f016863d91af750f9c537e70d
Loïc Donot <loic.donot@haute-garonne.gouv.fr>
2024-11-07 16:15:43 +0100

    Ajout scatter + amélioration print

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ let chart_evo;
 let graph_prod_conso;
 let chart_evo_init_w = 0;
 let graph_prod_conso_init_w = 0;
+let graph_prod_conso_init_h = 0;
 $(function() {
 
     window.addEventListener("beforeprint", (event) => {
         chart_evo_init_w = $('#chart_evo').width();
         graph_prod_conso_init_w = $('#graph_prod_conso').width();
+        graph_prod_conso_init_h = $('#graph_prod_conso').height();
         $('#graph_prod_conso').width('750px');
+        $('#graph_prod_conso').height('500px');
         $('#chart_evo').width('750px');
         chart_evo.reflow(); 
         graph_prod_conso.reflow();
@@ -15,6 +18,7 @@ $(function() {
     window.addEventListener("afterprint", (event) => {
         $('#chart_evo').width(chart_evo_init_w);
         $('#graph_prod_conso').width(graph_prod_conso_init_w);
+        $('#graph_prod_conso').height(graph_prod_conso_init_h);
         chart_evo.reflow(); 
         graph_prod_conso.reflow();
     });
@@ -272,17 +276,8 @@ $(function(){
             }
 
 
-            // couleurs
+
             let color = null;
-            /*if ( to == 'Rural autonome' ) {
-                color = '#4daf4a';
-            }
-            if ( to == 'Rural périurbain' ) {
-                color = '#984ea3';
-            } 
-            if ( to == 'Urbain' ) {
-                color = '#e41a1c';
-            } */
 
             let percent=0;
             if ( typ == 'conso_1' ) {
@@ -421,128 +416,244 @@ $(function(){
 
 });
 
-
-/* -- graph 1 
 $(function(){
 
-    let serieprod_conso = [];
-    for (let i in prod_conso) {
-        for (let j in prod_conso[i]) {
-            let from = prod_conso[i][j]['from'];
-            let to = prod_conso[i][j]['to'];
-            let weight = prod_conso[i][j]['weight'];
-            let typ = prod_conso[i][j]['typ'];
-            
-            // Traiter le cas des classes homonymes
-            if ( typ == 'prod_1' && to == 'Electricité' ) {
-                to = 'Prod. électricité';
-            }
-            if ( typ == 'prod_2' && from == 'Electricité' ) {
-                from = 'Prod. électricité';
-            }           
-            if ( from == 'Biogaz' ) {
-                from = 'Prod. biogaz';
-            }
-
-            // couleurs
-            let color = null;
-            if ( to == 'Rural autonome' ) {
-                color = '#4daf4a';
-            }
-            if ( to == 'Rural périurbain' ) {
-                color = '#984ea3';
-            } 
-            if ( to == 'Urbain' ) {
-                color = '#e41a1c';
-            } 
-
-            serieprod_conso.push([from, to, weight, color]);
+    const series = [{
+        name: 'Urbain',
+        id: 'urbain',
+        color: 'rgba(228, 26, 28, .7)',
+        data: [],
+        marker: {
+            symbol: 'square',
+            radius: 3
         }
-    }
+    },
+    {
+        name: 'Rural périurbain',
+        id: 'rural_periurbain',
+        color: 'rgba(152, 78, 163,.7)',
+        data: [],
+        marker: {
+            symbol: 'square',
+            radius: 3
+        }
+    },
+    {
+        name: 'Rural autonome',
+        id: 'rural_autonome',
+        color: 'rgba(77, 175, 74, .7)',
+        data: [],
+        marker: {
+            symbol: 'square',
+            radius: 3
+        }
+    }];
 
-    Highcharts.chart('graph_prod_conso', {
-
-        chart: {
-            backgroundColor: 'transparent',
-            margin:0,
-            spacing: [0,0,0,0]
-            //inverted: true
-        },
-    
-        title: {
-            text: undefined
-        },
-
-        tooltip:{
-            useHTML: true,
-            formatter: function() {
-                let tmpl = '';
-                if ( this.point.isNode === true ) { // Noeuds
-                    if (this.point.level < 2 ) { // Noeuds prod
-                        tmpl = `<strong>${this.point.name}:</strong><br><br>
-                        Production EnR: <strong style="font-size:1.8rem;">${Highcharts.numberFormat(this.point.sum)}</strong> GWh`;
-                        return tmpl;                    
-                    } else if( this.point.level == 2 ) {  // noeuds prod / conso
-                        const prod_enr = getsumweight(this.point.linksTo);
-                        const conso_enr = getsumweight(this.point.linksFrom);
-                        tmpl = `<strong>${this.point.name}:</strong>
-                        <table>
-                        <tr><td>Production EnR:</td><td style="text-align:right"><strong style="font-size:1.8rem;">${Highcharts.numberFormat(prod_enr)}</strong> GWh</td></tr>
-                        <tr><td>Consommation EnR: </td><td style="text-align:right"><strong style="font-size:1.8rem;">${Highcharts.numberFormat(conso_enr)}</strong> GWh</td></tr>
-                        </table>
-                        `;
-                        return tmpl; 
-                    } else { // Noeuds conso
-                        tmpl = `<strong>${this.point.name}:</strong><br><br>
-                        Consommation EnR: <strong style="font-size:1.8rem;">${Highcharts.numberFormat(this.point.sum)}</strong> GWh`;
-                        return tmpl;
-                    }
-                    
-                } else { // Liens entre noeuds
-                    console.log(this);
-                    let title = 'Production EnR';
-                    if ( this.point.toNode.level == 3 ) {
-                        title = 'Consommation EnR';
-                    }
-                    tmpl = `${title}<br><strong>${this.point.from} <span style="font-size:2rem; position:relative; top:5px;">⇝</span>
-                    ${this.point.to}:</strong><br>
-                    <div style="text-align:right">
-                        <strong style="font-size:1.8rem;">${Highcharts.numberFormat(this.point.weight)}</strong>GWh
-                    </div>`;
-                    return tmpl;
-                }
-            },
-        },
-    
-        series: [{
-            keys: ['from', 'to', 'weight', 'color'],
-            nodes: [
-                {id:'Rural autonome', color:'#4daf4a', width: 40, borderWidth: 2, borderColor: 'black'},
-                {id:'Rural périurbain', color:'#984ea3', width: 40, borderWidth: 2, borderColor: 'black'},
-                {id:'Urbain', color:'#e41a1c', width: 40, borderWidth: 2, borderColor: 'black'},
-                {id:'Production d\'agrocarburants', color:'#006065'},
-                {id:'Filière bois-énergie', color:'#ba7b3b'},
-                {id:'Solaire photovoltaïque', color:'#f7c631'},
-                {id:'Hydraulique renouvelable', color:'#0b5bb6'},
-                {id:'Incinération déchets - part EnR', color:'#72637f'},
-                {id:'Carburant ou combustible', color:'#004050'},
-                {id:'Géothermie très haute énergie', color:'#fe6a35'},
-                {id:'Géothermie profonde basse énergie', color:'#fe6a35'},
-                {id:'PACs géothermiques', color:'#fe6a35'},
-                {id:'Prod. électricité', color:'#2caffe'},
-                {id:'Chaleur', color:'#fe6a35'},
-                {id:'Electricité', color:'#2caffe'},
-                {id:'Bois-énergie (EnR)', color:'#ba7b3b'},
-            ],
-            data: serieprod_conso,
-            type: 'sankey',
-            name: 'Production et consommation d\'ENR par forme d\'EPCI'
-        }]
-    
+    pop_ratio.forEach(elm => {
+        let idx = 0;
+        if ( elm.forme == 'Rural périurbain' ) idx = 1;
+        if ( elm.forme == 'Rural autonome' ) idx = 2;
+        series[idx].data.push([elm.pmun_epci, elm.ratioenr, elm.siren_epci]);
+        //temp.push();
     });
     
+    const chart_pop_ratio = Highcharts.chart('pop_ratio', {
+        chart: {
+            type: 'scatter',
+            zooming: {
+                type: 'xy'
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: undefined,
+        },
+        xAxis: {
+            title: {
+                text: 'Population (hab.)'
+            },
+            labels: {
+                format: '{value:,.0f}',
+                rotation: -45
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true,
+            min:0,
+        },
+        yAxis: {
+            opposite: false,
+            title: {
+                text: 'Ratio EnR'
+            },
+            labels: {
+                format: '{value} %'
+            },
+            lineWidth: 1,
+            tickWidth: 1,
+            tickLength: 4,
+            max:300
+        },
+        legend: {
+            enabled: true
+        },
+        plotOptions: {
+            series: {
+                animation: false
+            },
+            scatter: {
+                marker: {
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                jitter: {
+                    x: 0.005
+                }
+            }
+        },
+        tooltip: {
+            useHTML:true,
+            formatter: function() {
+                const siren_epci = series[this.series.index]['data'][this.point.index][2];
+                const epci = idx_epci[siren_epci];
+                let tmpl = `
+                <div class="text-end">
+                    <strong class="badge fs-6" style="background-color:${this.color}">${this.series.name}</strong>
+                </div>
+                <table class="table mt-3">
+                    <tr>
+                        <th>EPCI</th>
+                        <th>Siège</th>
+                        <th>Pop.</th>
+                        <th>Ratio EnR</th>
+                    </tr>
+                    <tr>
+                        <td>${epci[0]}</td>
+                        <td><small>${epci[1]}</small></td>
+                        <td class="text-end">${Highcharts.numberFormat(this.point.x, 0)} hab.</td>
+                        <td class="text-end">${this.point.y} %</td>
+                    </tr>
+                </table>`;
+                return tmpl;
+            }
+            //pointFormat: 'Population: {point.x} hab <br/> Ratio EnR: {point.y} %'
+        },
+        series
+    });
 
-});*/
+    const chart_pop_ratio_zoom = Highcharts.chart('pop_ratio_zoom', {
+        chart: {
+            type: 'scatter',
+            zooming: {
+                type: 'xy'
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: undefined,
+        },
+        xAxis: {
+            title: {
+                text: 'Population (hab.)'
+            },
+            labels: {
+                format: '{value:,.0f}',
+                rotation: -45
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true,
+            min:0,
+            max: 125000
+        },
+        yAxis: {
+            title: {
+                text: 'Ratio EnR'
+            },
+            labels: {
+                format: '{value} %'
+            },
+            lineWidth: 1,
+            tickWidth: 1,
+            tickLength: 4,
+            max:100
+        },
+        legend: {
+            enabled: true
+        },
+        plotOptions: {
+            series: {
+                animation: false
+            },
+            scatter: {
+                marker: {
+                    radius: 2.5,
+                    symbol: 'circle',
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                jitter: {
+                    x: 0.005
+                }
+            }
+        },
+        tooltip: {
+            useHTML:true,
+            formatter: function() {
+                const siren_epci = series[this.series.index]['data'][this.point.index][2];
+                const epci = idx_epci[siren_epci];
+                let tmpl = `
+                <div class="text-end">
+                    <strong class="badge fs-6" style="background-color:${this.color}">${this.series.name}</strong>
+                </div>
+                <table class="table mt-3">
+                    <tr>
+                        <th>EPCI</th>
+                        <th>Siège</th>
+                        <th>Pop.</th>
+                        <th>Ratio EnR</th>
+                    </tr>
+                    <tr>
+                        <td>${epci[0]}</td>
+                        <td><small>${epci[1]}</small></td>
+                        <td class="text-end">${Highcharts.numberFormat(this.point.x, 0)} hab.</td>
+                        <td class="text-end">${this.point.y} %</td>
+                    </tr>
+                </table>`;
+                return tmpl;
+            }
+            //pointFormat: 'Population: {point.x} hab <br/> Ratio EnR: {point.y} %'
+        },
+        series
+    });
+
+});
 
 function getsumweight(arr_weight){
     return arr_weight.reduce((accumulator, current) => accumulator + current.weight, 0);
